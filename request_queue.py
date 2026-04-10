453ca50fde2d6258f6b694e4e2ef38038959e90f
Youssef Ibrahim <youssefml.ibrahim@gmail.com>
2026-04-10 14:00:19 -0600

    Update request_queue.py to add docstring

--- request_queue.py
+++ request_queue.py
@@ -2,16 +2,37 @@
 #doing this via a linked list implementation of a queue
 
 class RequestNode:
+    """
+    Represents a single node in the request queue, holding the contents of an individual request.
+    """
     def __init__(self, data : str):
+        """
+        Initializes a new RequestNode with the provided data.
+
+        Parameters:
+            data (str): the value of the request to hold in the RequestNode
+        """
         self.data = data
         self.next = None
 
 class RequestQueue:
+    """
+    A FIFO queue for processing incoming requests.
+    """
     def __init__(self):
+        """
+        Initializes an empty RequestQueue.
+        """
         self.head : RequestNode = None
         self.tail : RequestNode = None
     
     def enqueue(self, data : str):
+        """
+        Adds a new request with the provided data to the request queue (added at the end).
+
+        Parameters:
+            data (str): the content of the request to hold in the queue as a RequestNode
+        """
         new_node : RequestNode = RequestNode(data)
 
         if self.tail is not None:
@@ -21,9 +42,15 @@ class RequestQueue:
             self.head = new_node
 
     def dequeue(self):
+        """
+        Dequeues and returns request data at the front of the queue.
+
+        Returns:
+            The data inside the RequestNode at the head, detailing the request
+        """
         if self.head is None:
             return None
-        temp = self.head
+        temp : RequestNode = self.head
 
         self.head = self.head.next
         if self.head is None:
@@ -32,6 +59,12 @@ class RequestQueue:
         return temp.data
     
     def peek(self):
+        """
+        Returns request data at the front of the queue without dequeueing.
+
+        Returns:
+            The data inside the RequestNode at the head, detailing the request
+        """
         return self.head.data
 
 

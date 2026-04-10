ce52a8074f92d1839ff2440f4b4426173ffa2c4e
Youssef Ibrahim <youssefml.ibrahim@gmail.com>
2026-04-10 13:55:02 -0600

    Update request_queue.py with type hints

--- request_queue.py
+++ request_queue.py
@@ -2,17 +2,17 @@
 #doing this via a linked list implementation of a queue
 
 class RequestNode:
-    def __init__(self, data):
+    def __init__(self, data : str):
         self.data = data
         self.next = None
 
 class RequestQueue:
     def __init__(self):
-        self.head = None
-        self.tail = None
+        self.head : RequestNode = None
+        self.tail : RequestNode = None
     
-    def enqueue(self, data):
-        new_node = RequestNode(data)
+    def enqueue(self, data : str):
+        new_node : RequestNode = RequestNode(data)
 
         if self.tail is not None:
             self.tail.next = new_node
@@ -34,4 +34,6 @@ class RequestQueue:
     def peek(self):
         return self.head.data
 
+
+#testing
 
